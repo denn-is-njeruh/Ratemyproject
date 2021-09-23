07ba920dd00beb62f0465728e9a04b085b508f08
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-23 12:34:22 +0300

    add a ratings model to the application and register it to django-admin dashboard

--- ratings/models.py
+++ ratings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
@@ -7,9 +8,16 @@ class Project(models.Model):
   image = CloudinaryField('image', default='photo.jpeg')
   description = models.TextField(default='project description here')
   link = models.URLField(max_length=300, default='')
+  author = models.ForeignKey(User,on_delete=models.CASCADE, default='1')
 
   class Meta:
     ordering = ['title']
 
   def __str__(self):
     return self.title
+
+
+class Ratings(models.Model):
+  design = models.IntegerField(default=0)
+  usability = models.IntegerField(default=0)
+  content = models.IntegerField(default=0)
