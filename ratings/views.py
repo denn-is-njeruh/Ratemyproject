acd8af8033b28088f5d810ba9255e9581903a5cc
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-23 10:49:00 +0300

    add register_new_user view function to allow users to register when they want to use the application

--- ratings/views.py
+++ ratings/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,authenticate,logout
+from django.contrib import messages
+from .forms import NewUserForm
 
 # Create your views here.
 def index(request):
   message = "Welcome to Rate-My-Project"
   return render(request,'index.html',{"message": message})
+
+
+def register_new_user(request):
+  if request.method == "POST":
+    form = NewUserForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      messages.success(request, 'Registration Successful.')
+      return redirect('homepage')
+    messages.error(request, 'Unsuccessful registration. Invalid information.')
+  form = NewUserForm()
+  return render(request, 'registration/registration_form.html', {"registration_form": form} )
+
