6f08b274c0821969495c8efa605ea7c0c4222d4d
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-23 11:24:23 +0300

    add login view function to allow registered users to login in the application to view other user projects and view them

--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login,authenticate,logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import NewUserForm
 
@@ -19,5 +20,24 @@ def register_user(request):
       return redirect('homepage')
     messages.error(request, 'Unsuccessful registration. Invalid information.')
   form = NewUserForm()
-  return render(request, 'registration/registration_form.html', {"registration_form": form} )
+  return render(request, 'registration/registration_form.html', {"registration_form": form})
+
+
+def login_user(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+      user = authenticate(username=username,password=password)
+      if user is not None:
+        login(request, user)
+        messages.info(request, f"You are now logged in as {username}.")
+        return redirect('index')
+      else:
+        messages.error(request, f'Invalid Username or password')
+    else:
+      messages.error(request, f'Invalid username or password')
+  form = AuthenticationForm()
+  return render(request, 'registration/login.html', {"login_form": form})
 
