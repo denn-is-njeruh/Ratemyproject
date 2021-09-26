a877631716b29777a69ea61c9147c49d47f085c3
denn-is-njeruh <gitonjeruh@gmail.com>
2021-09-26 11:04:09 +0300

    add signals to the ratings app config file

--- ratings/apps.py
+++ ratings/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class RatingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ratings'
+    verbose_name = _('profiles')
+
+    def ready(self):
+        import ratings.signals
+
